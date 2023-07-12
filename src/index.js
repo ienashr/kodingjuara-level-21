ae7cf747b14bd297988bdb456ae4c4d33c81be6a
ienas harisah <118717624+ienashr@users.noreply.github.com>
2023-07-12 14:19:14 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+let bar = document.querySelector(".bars"),
+  navItem = document.querySelector(".nav-items");
+
+bar.addEventListener("click", () => {
+  navItem.classList.toggle("active");
+});
